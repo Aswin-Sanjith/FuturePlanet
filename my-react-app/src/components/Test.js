4bd0634cb4f93d7b1e553ebb871195e68203de35
AswinSanjith98 <aswin.sanjith@techversantinfo.com>
2025-02-25 01:41:05 +0530

    test

--- my-react-app/src/components/Test.js
+++ my-react-app/src/components/Test.js
@@ -7,7 +7,10 @@ import PropTypes from 'prop-types';
 const Test = () => {
   return (
     <div className="container mt-5">
-      <h1>This is a test file to test the release notes and the workflow, part 2, and the PR number</h1>
+      <p> I want to test the release notes and the workflow, part 2, and the PR number</p>
+      <button onClick={() => {
+        console.log('Button clicked');
+      }}>Click me</button>
     </div>
   );
 };
