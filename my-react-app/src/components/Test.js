4b007400c3c1d87662956e9e6d317c2ddf8eb7cb
AswinSanjith98 <aswin.sanjith@techversantinfo.com>
2025-02-24 18:35:13 +0530

    test

--- my-react-app/src/components/Test.js
+++ my-react-app/src/components/Test.js
@@ -9,6 +9,14 @@ const Test = () => {
     <div className="container mt-5">
       <h1>Test 1 </h1> 
       <h1>Test 2 </h1> 
+      <h1>Test 3 </h1> 
+      <h1>Test 4 </h1> 
+      <h1>Test 5 </h1> 
+      <h1>Test 6 </h1> 
+      <h1>Test 7 </h1> 
+      <h1>Test 8 </h1> 
+      <h1>Test 9 </h1> 
+      <h1>Test 10 </h1>  
     </div>
   );
 };
