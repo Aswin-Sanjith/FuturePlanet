bc6641c86efe8a06bef32b4401fdfd74228c9ce0
AswinSanjith98 <aswin.sanjith@techversantinfo.com>
2025-02-25 01:19:56 +0530

    test

--- my-react-app/src/components/Test.js
+++ my-react-app/src/components/Test.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const Test = () => {
   return (
     <div className="container mt-5">
-      <h1>technical changes, non-technical changes, and the PR number</h1>
+      <h1>new release, new workflow, new release notes</h1>
     </div>
   );
 };
