09b4a75b52d3740c65e3da1174a12b9705e26ee7
AswinSanjith98 <aswin.sanjith@techversantinfo.com>
2025-02-24 18:07:29 +0530

    Initial commit

---
+++ my-react-app/src/components/Test.js
@@ -0,0 +1,17 @@
+/**
+ * @component Test
+ * @description A simple component that displays "Hello World"
+ */
+import PropTypes from 'prop-types';
+
+const Test = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+Test.propTypes = {};
+
+export default Test; 
