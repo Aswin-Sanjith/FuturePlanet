892d475067a6c98b126de8e5a695748f400601bc
AswinSanjith98 <aswin.sanjith@techversantinfo.com>
2025-02-25 00:08:13 +0530

    test

--- my-react-app/src/components/Test.js
+++ my-react-app/src/components/Test.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const Test = () => {
   return (
     <div className="container mt-5">
-      <h1>This is a test file and was made to test the release notes and the workflow. This is the second attempt</h1>
+      <h1>please work</h1>
     </div>
   );
 };
